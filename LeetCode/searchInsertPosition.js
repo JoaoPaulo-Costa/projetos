b6310561428792be3b997cae8f2544f0b28dab5b
JoaoPaulo-Costa <costa.joao.paulo93@gmail.com>
2022-11-16 21:21:25 -0300

    binarySearch

---
+++ LeetCode/searchInsertPosition.js
@@ -0,0 +1,42 @@
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+// You must write an algorithm with O(log n) runtime complexity.
+
+let numeros = [];
+let target = 0;
+
+function searchInserts(numeros, target) {
+    for(i = 0; i < numeros.length; i++) {
+        if(target > numeros[numeros.length]) {
+            return (target - numeros[numeros.length]) + numeros.length;
+        }
+        if(numeros[i] === target){
+            return numeros.indexOf(numeros[i]);
+        }
+        if(numeros[i] > target){
+            return numeros.indexOf(numeros[i-1]) + 1;
+        }
+    }
+}
+
+function searchInsert(numeros, target) {
+
+    let lo = 0, hi = numeros.length;
+
+    while(lo < hi) { // breaks if lo == hi
+        let mid = lo + Math.floor((hi-lo)/2); // always gives the lower mid
+        if(target > numeros[mid]) { 
+            lo = mid + 1; // no way mid is a valid option
+        }
+        else {
+            hi = mid // it might be possibe to inseart @ mid
+        }
+    }
+    return lo;
+}
+
+// 0 _ 4 
+// 0 2 4
+// 3 _ 4
+// 3 3 4
+// 4 _ 4
+// 4
